17f07a3f3fcbbfbe9d1b4b92913ac3333fac7bce
Ratnadeep Simhadri <rsimhadri@paypal.com>
2022-11-22 17:23:03 -0800

    Sample route validation using express validator

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-
+import { body , check, oneOf, validationResult} from 'express-validator';
+import { handleInputErrors } from './modules/middleware'
 const router = Router();
 
 /**
@@ -11,8 +12,15 @@ router.get('/product',(req,res) => {
     })
 })
 router.get('/product/:id',() => {})
-router.put('/product/:id',() => {})
-router.post('/product',() => {})
+router.put('/product/:id', body('name').isString(), handleInputErrors , (req, res) => {
+    
+})
+router.post('/product', body('name').isString(), handleInputErrors, (req,res) => {
+    res.status(200)
+    res.json({
+        message: `Thanks for sharing your updates will create the product ${req.body.name} for you in the database`
+    })
+})
 router.delete('/product/:id',() => {})
 
 /**
