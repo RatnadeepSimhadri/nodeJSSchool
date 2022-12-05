b3026c7a24a586da8ac3d54fb9ce965f62a3c233
Ratnadeep Simhadri <rsimhadri@paypal.com>
2022-12-05 10:11:27 -0800

    Product Handlers

---
+++ src/handlers/product.ts
@@ -0,0 +1,89 @@
+import prisma from "../db"
+
+/**
+ * 
+ * @param req Get All Products
+ * @param res 
+ */
+export const getProducts = async (req, res) => {
+    
+    const user = await prisma.user.findUnique({
+        where: {
+            id: req.user.id
+        }, 
+        include: {
+            Product: true
+        }
+    }); 
+
+    res.json({data: user.Product})
+
+}
+
+/**
+ * Get a Product by ID
+ * @param req 
+ * @param res 
+ */
+export const getProduct  = async (req, res) => {
+    const { id } = req.params;
+    const product = await prisma.product.findFirst({
+        where: {
+            id : id,
+            belongsToId: req.user.id
+        }
+    });
+
+    res.json({data: product})
+
+}
+
+/**
+ * 
+ * @param req Create a Product
+ * @param res 
+ */
+export const createProduct = async (req, res) => {
+    const product = await prisma.product.create({
+        data: {
+            name: req.body.name,
+            belongsToId: req.user.id
+
+        }
+    })
+
+    res.json({data: product})
+}
+
+/**
+ * Update Information of a Specific Product
+ * @param req 
+ * @param res 
+ */
+export const updateProduct = async (req, res) => {
+    const update = await prisma.product.update({
+        where: {
+            id : req.params.id
+        }, 
+        data: {
+            name: req.body.name
+        }
+    }); 
+
+    res.json({data: update})
+}
+
+/**
+ * 
+ * @param req Delete a Product
+ * @param res 
+ */
+export const deleteProduct = async (req,res) => {
+    const product = await prisma.product.delete({
+        where : {
+            id: req.params.id, 
+        }
+    })
+
+    res.json({ data: product})
+}
